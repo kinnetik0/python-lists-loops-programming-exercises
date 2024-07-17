7615b6ea49a08ead9aa76a8372b40b71d345c875
Alexis Rosquete <alexis.rosquete@gmail.com>
2024-07-17 15:13:37 +0000

    commit

--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,3 +1,11 @@
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 # Your code here
+new_list = []
+
+for i in (my_list):
+    #print(f"Elemento: {i}, Tipo: {type(i)}")
+    if type(i) == list or type(i) == dict:
+        new_list.append(i)
+
+print(new_list)
--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,3 +1,12 @@
 my_list = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 # Your code here
+def sum_odds(list):
+    total = 0;
+    for value in list:
+        if value % 2 != 0:
+            total += value
+    return total
+
+
+print(sum_odds(my_list))
